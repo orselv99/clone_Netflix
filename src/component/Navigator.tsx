3085beaf602b6504fa92afb99cae65ba63410fe8
orsel <orselv99@gmail.com>
2021-08-01 23:52:20 +0900

    Navigator 애니메이션 추가

--- src/component/Navigator.tsx
+++ src/component/Navigator.tsx
@@ -1,36 +1,33 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./Navigator.css";
 
 export const Navigator = () => {
-  const [show, setShow] = useState(true);
-  const showNavigator = (visible: boolean): void => {
-    setShow(visible);
-  }
-  const scrollListener = () => {
-    if (window.scrollY > 100) {
-      showNavigator(true);
-    }
-    else {
-      showNavigator(false);
-    }
-  }
-  useEffect(() => {
-    window.addEventListener("scroll", scrollListener);
+  const [showBackground, setShowBackground] = useState(false);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 100) {
+        setShowBackground(true);
+      }
+      else {
+        setShowBackground(false);
+      }
+    }
+    //console.log(showBackground);
+    window.addEventListener("scroll", scrollListener);
     return () => window.removeEventListener("scroll", scrollListener);
-  }, []);
+  });
 
   return (
-    <div className={(show === true) ? "navigator" : "navigator_show_handler"}>
+    <div className={`navigator ${(showBackground === true) ? "navigator_show_background" : null}`}>
       <img 
         className="navigator_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         alt="Netflix"
       />
       
-      <div className="navigator_menu_center">
+      {/* <div className="navigator_menu_center">
         <button>Home</button>
         <button>TV Shows</button>
         <button>Movies</button>
@@ -43,7 +40,8 @@ export const Navigator = () => {
         <button>Shop</button>
         <button>Alarm</button>
         <button>Profile</button>
-      </div>
+      </div> */}
+
     </div>
   );
 };
